56673c331339921656be40b012d40ae719ee92d0
varinderpalsingh <varinder.palsingh@truinc.ca>
2022-05-24 00:04:38 +0530

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+const About: NextPage = () => {
+  return (
+    <>
+    About us
+    </>
+  )
+}
+
+export default About;
